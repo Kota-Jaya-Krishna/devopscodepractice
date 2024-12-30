7e5f8ac3416fba7cf6f1766d93af9db90eb55975
Kota-Jaya-Krishna <jayakrishnakota23@gmail.com>
2024-12-30 13:35:09 +0530

    added wife name

--- helloworld.sh
+++ helloworld.sh
@@ -5,8 +5,10 @@ COMPANY=DeutscheBank
 ROLE=Associate
 EXPERIENCE=5years
 TECHNOLOGY_LEARNING=devops_AWS
+MARRIED=harini
 
 echo $NAME
 echo $COMPANY
 echo $ROLE
 echo $TECHNOLOGY_LEARNING
+echo $MARRIED
